5a47c45491d23f37eba2c853acb20abc7a47d7cc
Eric-L-Araujo <ericaraujo77@hotmail.com>
2025-10-23 18:58:38 -0300

    teste de credenciais

--- app-cadastro/app/index.tsx
+++ app-cadastro/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
       {/* <ImageComponent /> */}
       <Text style = {style.text}>Tela de login</Text>
       <TextInput style={style.Input}
-      placeholder="Insira seu nome de usuárioo"
+      placeholder="Insira seu nome"
       
       />
        
